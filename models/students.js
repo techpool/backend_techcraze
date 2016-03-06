8fff71cef52a16aeed0561a2b25f455b87162bf0
Suryadeep Pal <suryadeep10@gmail.com>
2016-03-06 20:21:09 +0530

    student model created

---
+++ models/students.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
+
+var studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    emailId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    mobile: {
+        type: Number,
+        required: true
+    },
+    college: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    }
+});
+
+studentSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+studentSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('student', studentSchema);
